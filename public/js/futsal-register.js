63243ab3c72251c687c8794ff69b472009265a2f
Reuben27 <reuben.sd@iitgn.ac.in>
2021-10-10 05:22:51 +0530

    reload function

--- public/js/futsal-register.js
+++ public/js/futsal-register.js
@@ -1,8 +1,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyTzS1nvGVk6GiF42lQzxK8twM9e170sOgvA-gHBfc2SBK85CsGzgnqQnxgubd6Ou_q/exec';
 const form = document.forms['submit-to-google-sheet']
 form.addEventListener('submit', e => {
+  document.getElementById("loader_").style.display = "block";
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
+    .then(response => {
+      console.log('Success!', response);
+      document.getElementById("loader_").style.display = "none";
+      location.reload();
+    })
+    .catch(error => {console.error('Error!', error.message);
+    document.getElementById("loader_").style.display = "none";
+    location.reload();
+  })
 })
