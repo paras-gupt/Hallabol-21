1188af65bea70b2efa019b3f5f6dca8f8651ddda
paras-gupt <59513189+paras-gupt@users.noreply.github.com>
2021-10-10 22:28:27 +0530

    futsal page done ig

--- public/js/futsal.js
+++ public/js/futsal.js
@@ -17,3 +17,40 @@ function menuBtnChange() {
     closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the iocns class
   }
 }
+
+var currWidth = window.matchMedia("(max-width: 700px)")
+function sidebar_open() {
+  var x = document.getElementById("navHorList");
+  var open = false;
+  if(window.getComputedStyle(x).display === "none"){
+    console.log('hey')
+    open = true;
+  }
+    console.log(open)
+    if(open == true){
+      console.log(screen.width);
+      if (screen.width < 700) { // If media query matches
+        console.log('hy');
+        setTimeout(() => {console.log("haha");}, 1000);
+        document.getElementById("navHorList").style.display = "block";
+        open = false;
+      } 
+    } else{
+      if (screen.width < 700) { // If media query matches
+        console.log('hyasjhadffdkh');
+        document.getElementById("navHorList").style.display = "none";
+        open = true;
+      } 
+    }
+
+    
+}
+
+// sidebar_open(currWidth)
+// currWidth.addEventListener(sidebar_open)
+
+// function sidebar_close() {
+//     document.getElementById("main").style.marginLeft = "0%";
+//     document.getElementById("mySidebar").style.display = "none";
+//     document.getElementById("openNav").style.display = "inline-block";
+// }
